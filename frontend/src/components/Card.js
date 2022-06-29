d051b22c2aa15604229270b9b56f7f8ae5df7e88
delioncourts <delioncourtles@yandex.ru>
2022-06-29 23:27:34 +0300

    upd likes

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -6,6 +6,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
 
     // Определяем, являемся ли мы владельцем текущей карточки
     const isOwn = card.owner._id === currentUser._id || card.owner === currentUser._id;
+   // const isOwn = card.owner === currentUser._id;
 
     // Создаём переменную, которую после зададим в `className` для кнопки удаления
     const cardDeleteButtonClassName = (
@@ -13,8 +14,8 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
     );
 
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = card.likes.some(i => i._id === currentUser._id || i._id === currentUser._id);
-
+    //const isLiked = card.likes.some(i => i._id === currentUser._id || i._id === currentUser._id);
+     const isLiked = card.likes.some(i => i === currentUser._id || i._id === currentUser._id);
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = `card__like ${isLiked ? 'card__like-active' : ''} `;
 
