c285e2dd93e8d8b232d66b2b6320ebe009b1f5ef
delioncourts <delioncourtles@yandex.ru>
2022-07-01 20:38:35 +0300

    upd header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from "../images/logo.svg";
 
-console.log(Hello)
+console.log('Hello')
 
 function Header({ userEmail, onSignOut }) {
   const location = useLocation();
